ed0c253df7f68641a4377e72b132bc6c4f9e17e2
Kathy Kam <2896293+kathykam@users.noreply.github.com>
2024-10-11 17:34:12 -0700

    In CDK we need to add comments on each property explaining the purpose of it, and if we provide a default value for it in CDK. These comments are used to generate the Constructs docs.

--- packages/aws-cdk-lib/aws-elasticache/lib/elasticache-cluster.ts
+++ packages/aws-cdk-lib/aws-elasticache/lib/elasticache-cluster.ts
@@ -22,38 +22,166 @@ export enum CacheClusterStatus {
 }
 
 export interface ElastiCacheClusterProps {
+  /**
+   * The VPC where the cache cluster will be deployed.
+   */
   readonly vpc: ec2.IVpc;
+
+  /**
+   * The name of the cache engine to be used for this cache cluster.
+   */
   readonly engine: CacheEngine;
+
+  /**
+   * The compute and memory capacity of the nodes in the cache cluster.
+   * @see https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/CacheNodes.SupportedTypes.html
+   */
   readonly cacheNodeType: string;
+
+  /**
+   * The number of cache nodes in the cache cluster.
+   */
   readonly numCacheNodes: number;
 
+  /**
+   * A name for the cache cluster. If you don't specify a name, AWS CloudFormation generates a unique physical ID and uses that ID for the cache cluster.
+   * @default - AWS CloudFormation generated name
+   */
   readonly clusterName?: string;
+
+  /**
+   * The version number of the cache engine.
+   * @default - latest version
+   */
   readonly engineVersion?: string;
+
+  /**
+   * The port number on which the cache accepts connections.
+   * @default - 6379 for Redis, 11211 for Memcached
+   */
   readonly port?: number;
+
+  /**
+   * The subnet group where the cache cluster will be placed.
+   * @default - a new subnet group will be created
+   */
   readonly subnetGroup?: elasticache.CfnSubnetGroup;
+
+  /**
+   * The security groups to assign to the cache cluster.
+   * @default - a new security group will be created
+   */
   readonly securityGroups?: ec2.ISecurityGroup[];
+
+  /**
+   * The name of the parameter group to associate with this cache cluster.
+   * @default - the default parameter group for the specified engine
+   */
   readonly cacheParameterGroupName?: string;
+
+  /**
+   * Indicates that minor engine upgrades will be applied automatically to the cache cluster during the maintenance window.
+   * @default true
+   */
   readonly autoMinorVersionUpgrade?: boolean;
+
+  /**
+   * Specifies whether the nodes in this Memcached cluster are created in a single Availability Zone or created across multiple Availability Zones in the cluster's region.
+   * @default - single Availability Zone
+   */
   readonly azMode?: string;
+
+  /**
+   * The EC2 Availability Zone in which the cache cluster is created.
+   * @default - random Availability Zone in the region
+   */
   readonly preferredAvailabilityZone?: string;
+
+  /**
+   * A list of the Availability Zones in which cache nodes are created.
+   * @default - Availability Zones are automatically chosen
+   */
   readonly preferredAvailabilityZones?: string[];
+
+  /**
+   * The weekly time range (in UTC) during which system maintenance can occur.
+   * @default - 60-minute window selected at random from an 8-hour block of time per region, occurring on a random day of the week
+   */
   readonly preferredMaintenanceWindow?: string;
+
+  /**
+   * The Amazon Resource Name (ARN) of the Amazon Simple Notification Service (SNS) topic to which notifications are sent.
+   * @default - no notifications are sent
+   */
   readonly notificationTopicArn?: string;
+
+  /**
+   * A list of Amazon Resource Names (ARN) that uniquely identify the Redis RDB snapshot files stored in Amazon S3. The snapshot files are used to populate the new cache cluster.
+   * @default - cache cluster is not created from a snapshot
+   */
   readonly snapshotArns?: string[];
+
+  /**
+   * The name of a Redis snapshot from which to restore data into the new cache cluster.
+   * @default - cache cluster is not created from a snapshot
+   */
   readonly snapshotName?: string;
+
+  /**
+   * The number of days for which ElastiCache retains automatic snapshots before deleting them.
+   * @default - 0 (automatic backups are disabled)
+   */
   readonly snapshotRetentionLimit?: number;
+
+  /**
+   * The daily time range (in UTC) during which ElastiCache begins taking a daily snapshot of your cache cluster.
+   * @default - 60-minute window selected at random from an 8-hour block of time per region
+   */
   readonly snapshotWindow?: string;
+
+  /**
+   * An array of key-value pairs to apply to this cache cluster.
+   * @default - no tags
+   */
   readonly tags?: { [key: string]: string };
+
+  /**
+   * Specifies whether encryption is enabled for the cache cluster.
+   * @default - encryption is disabled
+   */
   readonly encryption?: {
+    /**
+     * Indicates whether to enable encryption at rest.
+     */
     atRest: boolean;
+    /**
+     * Indicates whether to enable encryption in transit.
+     */
     inTransit: boolean;
+    /**
+     * The KMS key to use for encryption.
+     * @default - AWS owned CMK
+     */
     kmsKey?: kms.IKey;
   };
+
+  /**
+   * Specifies the backup configuration for the cache cluster.
+   * @default - no backups
+   */
   readonly backups?: {
+    /**
+     * The number of days for which ElastiCache retains automatic snapshots before deleting them.
+     */
     retention: Duration;
+    /**
+     * The daily time range during which automated backups are created.
+     * @default - 60-minute window selected at random from an 8-hour block of time per region
+     */
     preferredWindow?: string;
   };
 }
+
 
 export class ElastiCacheCluster extends Resource {
   public readonly cluster: elasticache.CfnCacheCluster;
